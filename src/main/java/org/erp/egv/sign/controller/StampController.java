db429fb629bf130e8384a9db884b74f670e9a893
kimjiwon <33cattty@gmail.com>
2021-11-22 03:58:20 +0900

    Add : /sign/stamp 기본틀 생성

---
+++ src/main/java/org/erp/egv/sign/controller/StampController.java
@@ -0,0 +1,13 @@
+package org.erp.egv.sign.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/sign/*")
+public class StampController {
+	
+	@GetMapping("/stamp")
+	public void getStamp() {}
+}

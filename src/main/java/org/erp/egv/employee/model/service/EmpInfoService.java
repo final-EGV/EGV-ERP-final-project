a02065270c5e49f98462cfa9531112bba0e0b70d
sunmin <smksm08@naver.com>
2021-11-22 21:03:37 +0900

    Feat : 사번 찾기 기능 추가 및 시큐리티 주석 처리
    사번 찾기 추가 및 시큐리티 주석 처리

--- src/main/java/org/erp/egv/employee/model/service/EmpInfoService.java
+++ src/main/java/org/erp/egv/employee/model/service/EmpInfoService.java
@@ -1,6 +1,8 @@
 package org.erp.egv.employee.model.service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.erp.egv.employee.model.dao.EmpInfoDAO;
 import org.erp.egv.employee.model.dto.EmployeeDTO;
@@ -19,6 +21,30 @@ public class EmpInfoService {
 	public List<EmployeeDTO> empListRequest() {
 		System.out.println("서비스로 오나요?");
 		return empInfoDAO.empListRequest();
+	}
+
+	public Map<String, String> finId(String name, String birtha, String email) {
+		EmployeeDTO emp = empInfoDAO.finId(email);
+		String code = null;
+		Map<String, String> result = new HashMap<>(); 
+		
+		if(emp != null) {
+			System.out.println("이메일 일치");
+			if (emp.getName().equals(name)) {
+				System.out.println("이름 일치");
+				if(emp.getRrn().substring(0, 6).equals(birtha)) {
+					System.out.println("생일 일치");
+					code = emp.getCode();
+				}
+			}
+		}
+		
+		if(code != null) {
+			result.put("code", code);
+			result.put("date", String.valueOf(emp.getEntDate()));
+		}
+		
+		return result;
 	}
 
 

30cd73a0e8ceb3152ba70c994d48ee81061dbb72
kimjiwon <33cattty@gmail.com>
2021-11-29 15:16:06 +0900

    fix : 기안서 작성 유효성검사

--- src/main/java/org/erp/egv/sign/controller/StampController.java
+++ src/main/java/org/erp/egv/sign/controller/StampController.java
@@ -20,7 +20,6 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-
 @Controller
 @RequestMapping("/sign/*")
 public class StampController {

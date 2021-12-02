f8626bcc281bc753bfbe97e27da54466e56b2b9e
JuHoKim <kjh00345@gmail.com>
2021-12-02 19:11:46 +0900

    Feat: 연차 페이지만 추가

--- src/main/java/org/erp/egv/leave/controller/LeaveController.java
+++ src/main/java/org/erp/egv/leave/controller/LeaveController.java
@@ -1,9 +1,14 @@
 package org.erp.egv.leave.controller;
 
+import java.util.List;
+
+import org.erp.egv.leave.model.dto.UseAnnualLeaveDTO;
 import org.erp.egv.leave.model.service.LeaveService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/emp")
@@ -14,5 +19,15 @@ public class LeaveController {
 	@Autowired
 	public LeaveController(LeaveService leaveService) {
 		this.leaveService = leaveService;
+	}
+	
+	@GetMapping("/leave")
+	public ModelAndView workList(ModelAndView mv) {
+		List<UseAnnualLeaveDTO> leaveList = leaveService.leaveList();
+	
+		mv.addObject("leave", leaveList);
+		mv.setViewName("/emp/work/annualLeave");
+		
+		return mv;
 	}
 }
--- src/main/java/org/erp/egv/leave/model/service/LeaveService.java
+++ src/main/java/org/erp/egv/leave/model/service/LeaveService.java
@@ -1,6 +1,9 @@
 package org.erp.egv.leave.model.service;
 
+import java.util.List;
+
 import org.erp.egv.leave.model.dao.LeaveDAO;
+import org.erp.egv.leave.model.dto.UseAnnualLeaveDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,5 +15,10 @@ public class LeaveService {
 	@Autowired
 	public LeaveService(LeaveDAO leaveDAO) {
 		this.leaveDAO = leaveDAO;
+	}
+
+	public List<UseAnnualLeaveDTO> leaveList() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }

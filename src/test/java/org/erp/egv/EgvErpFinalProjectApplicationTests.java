f5c68ac4b3e1726c1ac20084348944a08c2ff153
UnSuk Song <darkasassin@naver.com>
2022-02-12 00:36:28 +0900

    Chore: jar 방식 빌드 설정

--- src/test/java/org/erp/egv/EgvErpFinalProjectApplicationTests.java
+++ src/test/java/org/erp/egv/EgvErpFinalProjectApplicationTests.java
@@ -1,9 +1,10 @@
 package org.erp.egv;
 
+import org.erp.egv.configuration.EgvErpFinalProjectApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = EgvErpFinalProjectApplication.class)
 class EgvErpFinalProjectApplicationTests {
 
 	@Test

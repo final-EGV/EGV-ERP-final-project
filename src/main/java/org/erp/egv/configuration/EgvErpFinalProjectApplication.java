73a8a6e0e8db6f52383ea0ed0a4ef37e1c34bd01
UnSuk Song <darkasassin@naver.com>
2022-04-02 02:09:51 +0900

    Chore: 외부 Tomcat 배포용 빌드 설정
    - 프로젝트 패키징 방식 war로 변경.
    - Spring 내장 tomcat 외, 별도 외부 tomcat 사용 설정.
    - Tomcat 로깅용 properties 파일 추가.

--- src/main/java/org/erp/egv/configuration/EgvErpFinalProjectApplication.java
+++ src/main/java/org/erp/egv/configuration/EgvErpFinalProjectApplication.java
@@ -3,15 +3,23 @@ package org.erp.egv.configuration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "org.erp.egv")
 @EntityScan(basePackages = "org.erp.egv")
-public class EgvErpFinalProjectApplication {
+public class EgvErpFinalProjectApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EgvErpFinalProjectApplication.class, args);
+	}
+	
+	/** for external Apache Tomcat war deploy */
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(EgvErpFinalProjectApplication.class);
 	}
 
 }

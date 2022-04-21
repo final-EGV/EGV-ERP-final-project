66cbfe4d98fbd3c2bd28a265894729e9ab8ec6e7
UnSuk Song <darkasassin@naver.com>
2022-04-22 02:08:54 +0900

    Chore: Spring AOP를 사용한 로깅 환경 구성

--- src/main/java/org/erp/egv/configuration/EgvErpFinalProjectApplication.java
+++ src/main/java/org/erp/egv/configuration/EgvErpFinalProjectApplication.java
@@ -6,10 +6,12 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "org.erp.egv")
 @EntityScan(basePackages = "org.erp.egv")
+@EnableAspectJAutoProxy
 public class EgvErpFinalProjectApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {

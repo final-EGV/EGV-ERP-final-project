208180140f6dd833e91f722a2e48f1e556ced439
UnSuk Song <darkasassin@naver.com>
2022-04-28 02:01:03 +0900

    Style: 오타 수정

--- src/main/java/org/erp/egv/aop/logging/LoggingMethodTermination.java
+++ src/main/java/org/erp/egv/aop/logging/LoggingMethodTermination.java
@@ -43,7 +43,7 @@ public class LoggingMethodTermination {
 
 		Signature signature = jp.getSignature();
 
-		LOGGER.info("##### [TERMINATED] : {}.{}() with returning : {}",
+		LOGGER.info("##### [TERMINATED] : {}.{}() with returning {}",
 				signature.getDeclaringTypeName(),
 				signature.getName(),
 				result != null ? result.toString() : null);
